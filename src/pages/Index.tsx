c383fec97667c462881d5b7b1b6227ddf20c787a
GouravPatidar91 <127935109+GouravPatidar91@users.noreply.github.com>
2025-03-27 23:52:24 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -61,7 +61,7 @@ const Index = () => {
           </div>
           
           <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
-            Welcome to QuizVerse of SCSIT
+            Welcome to Kon Banega Coder (KBC) of Ecell
           </h1>
           
           {userTeam ? (
